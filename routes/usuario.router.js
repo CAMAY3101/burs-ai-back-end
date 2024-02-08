3e9d08a4178aba30648bd07fedde6dcd9c2ffd10
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-02-07 22:39:07 -0600

    phone verify route
    Fixes #22

--- routes/usuario.router.js
+++ routes/usuario.router.js
@@ -6,5 +6,6 @@ const usuarioController = require('../controllers/usuario.controller');
 usuarioRouter.get('/getUsuarios', usuarioController.getUsuarios);
 usuarioRouter.post('/addUsuario', usuarioController.addUsuario);
 usuarioRouter.post('/verifyEmail', usuarioController.verifyEmail);
+usuarioRouter.post('/verifyPhoneNumber', usuarioController.verifyPhoneNumber);
 
 module.exports = usuarioRouter; 
