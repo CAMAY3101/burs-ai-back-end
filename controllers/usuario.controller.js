ce098cc068c9ba50fade41d8126daa120dc07257
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-03-13 13:37:10 -0600

    resend otp code
    Fixes #45

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -36,8 +36,8 @@ const usuarioController = {
             // Configurar la cookie con el token
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production', // Usar secure en producción
-                sameSite: 'strict',
+                //secure: process.env.NODE_ENV === 'production', // Usar secure en producción
+                //sameSite: 'strict',
                 maxAge: 24 * 60 * 60 * 1000 // Duración de 1 día
             });
             
@@ -88,6 +88,38 @@ const usuarioController = {
             errorUpdate.statusCode = 500;
             errorUpdate.status = 'error';
             next(errorUpdate);
+        }
+    },
+    resendOTPCodeEmail: async (req, res, next) => {
+        try {
+            const userId = req.user.id_usuario;
+            const emailModel = await usuarioModel.getEmailUser(userId);
+            await twilioService.sendOTP_Email(emailModel.correo);
+            res.status(200).json({
+                status: 'success',
+                message: 'Código de verificación reenviado con éxito'
+            });
+        } catch (error) {
+            const errorResendEmail = new Error();
+            errorResendEmail.statusCode = 500;
+            errorResendEmail.status = 'error';
+            next(errorResendEmail);
+        }
+    },
+    resendOTPCodePhoneNumber: async (req, res, next) => {
+        try {
+            const userId = req.user.id_usuario;
+            const phoneModel = await usuarioModel.getPhoneUser(userId);
+            await twilioService.sendOTP_PhoneNumber(phoneModel.telefono);
+            res.status(200).json({
+                status: 'success',
+                message: 'Código de verificación reenviado con éxito'
+            });
+        } catch (error) {
+            const errorResendPhone = new Error();
+            errorResendPhone.statusCode = 500;  
+            errorResendPhone.status = 'error';
+            next(errorResendPhone);
         }
     },
 
--- routes/usuario.router.js
+++ routes/usuario.router.js
@@ -8,6 +8,8 @@ usuarioRouter.get('/getUsuarios', usuarioController.getUsuarios);
 usuarioRouter.post('/createUser', usuarioController.createUser);
 
 usuarioRouter.post('/updateDataUser', authenticateJWT, usuarioController.updateDataUser);
+usuarioRouter.post('/resendOTPCodeEmail', authenticateJWT, usuarioController.resendOTPCodeEmail);
+usuarioRouter.post('/resendOTPCodePhoneNumber', authenticateJWT, usuarioController.resendOTPCodePhoneNumber);
 usuarioRouter.post('/verifyEmail', authenticateJWT, usuarioController.verifyEmail);
 usuarioRouter.post('/verifyPhoneNumber', authenticateJWT, usuarioController.verifyPhoneNumber);
 
