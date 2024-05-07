057ef230d0c04ece511a2915788b601bd088529c
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-05-07 13:49:15 -0600

    Last updates
    Close #49
    Close #50
    Close #51
    Close #52

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,22 @@ const router = express.Router()
 
 router.use('/usuarios', require('./usuario.router'))
 
+
+router.get('/check-cookie', (req, res) => {
+    try {
+        // Verificar si la cookie está presente en la solicitud
+        if (req.cookies.token) {
+            // La cookie está presente, enviar una respuesta con un indicador de éxito
+            res.json({ tokenExist: true });
+        } else {
+            // La cookie no está presente, enviar una respuesta con un indicador de fallo
+            res.json({ tokenExist: false });
+        }
+    } catch (error) {
+        console.log("Error en check-cookie");
+        console.log(error);
+        res.json({ error: error });
+    }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,35 +4,40 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv').config();
 const errorHandler = require('./middlewares/errorHandler');
 
-const {db} = require('./services/db.server');
+const { db } = require('./services/db.server');
 
 const port = process.env.PORT
 
-const app = express();1
+const app = express(); 1
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://burs.com.mx"],
-    credentials: true
+    origin: [process.env.FRONTEND_URL_1, process.env.FRONTEND_URL_2],
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
 }));
 
+
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://burs.com.mx");
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL_1);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'content-type');
+    res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 
 //Get principal
 app.get('/', (req, res, next) => {
-    try{
+    try {
         const result = db.query('SELECT NOW()');
-        res.json({result})
-    }catch (error){
+        res.json({ message: 'hello' })
+    } catch (error) {
         console.log("Error en get principal");
         console.log(error);
-        res.json({error: error});
+        res.json({ error: error });
     }
 })
 
