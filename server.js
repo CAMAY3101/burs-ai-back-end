5f228cb42637a58ac5189cf3b2b0e2c8672a9a85
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-11-09 12:53:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL_1);
+    res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL_2 : process.env.FRONTEND_URL_1);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'content-type');
