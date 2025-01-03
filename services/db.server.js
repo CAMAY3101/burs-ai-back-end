0e95c6cedfddd9fcc31e46aecc92db2b7873559c
Aylin Camacho <58498498+CAMAY3101@users.noreply.github.com>
2025-01-03 16:17:21 -0600

    WIP

--- services/db.server.js
+++ services/db.server.js
@@ -1,27 +1,22 @@
-// variables base de datos
-const { DB_URL, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
-//const pgp = require('pg-promise')()
-
-//const db = pgp('postgres://postgres:m19m31a31@localhost:5432/dummy_test')
-
 const pgp = require('pg-promise')();
+
+const { DB_USERNAME2, DB_PASSWORD2, DB_HOST2, DB_PORT2, DB_NAME2 } = process.env;
 
 let db;
 
 const cn = {
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME,
-    user: DB_USERNAME,
-    password: DB_PASSWORD,
+    host: DB_HOST2,
+    port: DB_PORT2,
+    database: DB_NAME2,
+    user: DB_USERNAME2,
+    password: DB_PASSWORD2,
     ssl: {
-        rejectUnauthorized: false // Acepta certificados autofirmados (opcional, úsalo solo si es necesario)
+        rejectUnauthorized: false
     }
 };
 
 try {
     db = pgp(cn);
-    //db = pgp('postgres://postgres:m19m31a31@localhost:5432/dummy_test')
     console.log("Conexión a la base de datos establecida con éxito.");
 } catch (e) {
     console.error("Error al conectar a la base de datos:", e);
