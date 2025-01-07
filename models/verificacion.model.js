32c02475f8e2b3658d7926172dea7b2ac7eca516
Aylin Camacho <58498498+CAMAY3101@users.noreply.github.com>
2025-01-06 21:19:39 -0600

    Update verificacion.model.js

--- models/verificacion.model.js
+++ models/verificacion.model.js
@@ -4,7 +4,8 @@ const {db} = require('../services/db.server');
 const verificacionModel = {
 
     updateEmailVerificationStatus: async (userId, status) => {
-        return await db.none('INSERT INTO verification (uuid_client, verificacion_correo) VALUES ($1, $2)', [userId, status]);
+        const uuid = uuidv4();
+        return await db.none('INSERT INTO verification (uuid_verification, uuid_client, verificacion_correo) VALUES ($1, $2, $3)', [uuid, userId, status]);
     },
     updatePhoneVerificationStatus: async (userId, status) => {
         return await db.none('UPDATE verification SET verificacion_telefono = $1 WHERE uuid_client = $2', [status, userId]);
